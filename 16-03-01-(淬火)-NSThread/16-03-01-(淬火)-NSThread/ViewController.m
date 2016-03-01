37c6f9609c9f612210fa399247de01697328e368
陈栋 <chendong@yitaosoft.com>
2016-03-01 16:24:42 +0800

    线程创建的三种方式
    线程创建的三种方式  NSThread

--- 16-03-01-(淬火)-NSThread/16-03-01-(淬火)-NSThread/ViewController.m
+++ 16-03-01-(淬火)-NSThread/16-03-01-(淬火)-NSThread/ViewController.m
@@ -15,6 +15,53 @@
 @implementation ViewController
 
 
+- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event
+{
+    [self test3];
+}
 
+
+//线程执行方法内容
+- (void) run:(NSString *) str
+{
+    NSLog(@"%@",str);
+    for (int i=0; i<10; i++) {
+        NSLog(@"%s---%@",__func__,[NSThread currentThread]);
+    }
+}
+
+
+
+#pragma 线程创建的方式
+
+/**
+ *  创建方式3   
+ *  隐式创建  无需启动线程
+ */
+- (void) test3
+{
+    [self performSelectorInBackground:@selector(run:) withObject:@"test3"];
+}
+
+
+/**
+ *  创建方式2  无需启动线程
+ */
+- (void) test2
+{
+    [NSThread detachNewThreadSelector:@selector(run:) toTarget:self withObject:@"haha"];
+}
+
+
+//创建方式1
+- (void) test1
+{
+    //实例化一个线程对象
+    NSThread *thread = [[NSThread alloc] initWithTarget:self selector:@selector(run) object:nil];
+    
+    //启动线程
+    [thread start];
+    
+}
 
 @end

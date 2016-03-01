85fb93953565e6e1bd0f455a07049ff085e68e2d
陈栋 <chendong@yitaosoft.com>
2016-03-01 17:01:33 +0800

    线程状态练习
    线程状态练习

--- 16-03-01-(淬火)-NSThrea线程状态(知道)/16-03-01-(淬火)-NSThrea线程状态(知道)/ViewController.m
+++ 16-03-01-(淬火)-NSThrea线程状态(知道)/16-03-01-(淬火)-NSThrea线程状态(知道)/ViewController.m
@@ -34,12 +34,29 @@
 - (void)run
 {
     NSLog(@"%s",__func__);
+    //刚进来就睡眠2秒
+//    [NSThread sleepForTimeInterval:3.0];
+    //睡到指定时间点
+//    [NSThread sleepUntilDate:[NSDate dateWithTimeIntervalSinceNow:5.0]];
     
     
     for (int i = 0; i < 20; i++) {
+    
+        //满足一定条件后，让线程休息一会儿
+        if (i==10) {
+             [NSThread sleepForTimeInterval:3.0];
+        }
         
-\
+        //满足一定条件  结束线程
+        if (i==15) {
+            NSLog(@"线程要结束了哦！！");
+            [NSThread exit];
+        }
+        
+        NSLog(@"%@--- %d", [NSThread currentThread], i);
     }
+    
+     NSLog(@"线程结束");
 
 }
 

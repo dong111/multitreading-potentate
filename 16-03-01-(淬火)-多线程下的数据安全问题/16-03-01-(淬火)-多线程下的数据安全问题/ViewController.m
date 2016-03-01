27b578ca9341493629a1e8914c57c816921b691b
陈栋 <chendong@yitaosoft.com>
2016-03-01 17:25:15 +0800

    模拟售票员 售票
    模拟售票员 售票
    出现出现相同售票问题

--- 16-03-01-(淬火)-多线程下的数据安全问题/16-03-01-(淬火)-多线程下的数据安全问题/ViewController.m
+++ 16-03-01-(淬火)-多线程下的数据安全问题/16-03-01-(淬火)-多线程下的数据安全问题/ViewController.m
@@ -10,18 +10,53 @@
 
 @interface ViewController ()
 
+
+//总票数  模拟售票员 售票
+@property (assign,nonatomic) int tickets;
+
 @end
 
 @implementation ViewController
 
-- (void)viewDidLoad {
-    [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event
+{
+
+    //剩余票数
+    self.tickets = 20;
+    
+    NSThread *threadA = [[NSThread alloc] initWithTarget:self selector:@selector(saleTicktes) object:nil];
+    
+    threadA.name = @"售票员A";
+    [threadA start];
+    
+    NSThread *threadB = [[NSThread alloc] initWithTarget:self selector:@selector(saleTicktes) object:nil];
+    
+    threadB.name = @"售票员B";
+    [threadB start];
+    
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+
+#pragma --mark 提供售票方法
+- (void) saleTicktes
+{
+    while (YES) {
+        //模拟下一张票，卖一张票休息一秒
+        [NSThread sleepForTimeInterval:1.0];
+        
+        //判断是否有票
+        if (self.tickets>0) {
+            self.tickets --;
+            NSLog(@"剩余票数:--%d---%@",self.tickets,[ NSThread currentThread]);
+            
+        }else{
+            NSLog(@"票卖完了！！");
+            break;
+        }
+
+        
+    }
 }
+
 
 @end

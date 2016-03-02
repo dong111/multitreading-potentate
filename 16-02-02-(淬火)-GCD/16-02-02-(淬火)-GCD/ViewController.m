661c53bd01a91254043583000e0cce81ea605c89
陈栋 <chendong@yitaosoft.com>
2016-03-02 10:09:53 +0800

    GCD
    grand center dispatch

---
+++ 16-02-02-(淬火)-GCD/16-02-02-(淬火)-GCD/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  16-02-02-(淬火)-GCD
+//
+//  Created by 陈栋 on 16/3/2.
+//  Copyright © 2016年 man. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+//GCD核心概念
+//任务：block
+//队列:把任务放到队列里面，队列是先进先出的的原则
+//串行队列:顺序,一个个执行(必须一个任务执行完，才能从队列里面取出下一个任务)
+//并行队列:同时，同时执行多个任务（可以同时取出多个任务，只要线程执行）
+
+//同步sync:
+
+
+
+@end
---
+++ 16-02-02-(淬火)-GCD/16-02-02-(淬火)-GCD/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  16-02-02-(淬火)-GCD
+//
+//  Created by 陈栋 on 16/3/2.
+//  Copyright © 2016年 man. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
